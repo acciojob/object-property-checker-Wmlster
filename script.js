4bf58f7720a05994a54e3fbfafa7a8537292c6f7
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-10-25 15:45:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 function hasKey(key) {
   //   write your code here
 
-	if(sampleObject.key==red||sampleObject.key==green||sampleObject.key==white)
+	if(sampleObject.key=="#FF0000"||sampleObject.key=="#00FF00"||sampleObject.key=="#FFFFFF")
 		return true;
 	return false;
 }
